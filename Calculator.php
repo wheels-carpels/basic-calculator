42888ffc385e1bc61d8f62d9b4a9c62723a5f96e
Linda <152405803+wheels-carpels@users.noreply.github.com>
2023-12-01 15:47:32 +0600

    Create Calculator.php

---
+++ Calculator.php
@@ -0,0 +1,24 @@
+<?php
+// Calculator.php
+class Calculator {
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+
+    public function divide($a, $b) {
+        if ($b !== 0) {
+            return $a / $b;
+        } else {
+            return "Error: Division by zero";
+        }
+    }
+}
+?>
